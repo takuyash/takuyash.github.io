0542305ed93fcc2a8a106c618a3430382701f540
takuyash <14888534+takuyash@users.noreply.github.com>
2019-07-15 09:25:51 +0900

    ポートフォリオの新規作成

---
+++ js/json.js
@@ -0,0 +1,16 @@
+var works = [
+    {'id':'work1','title':'利用者別NAPT',
+    'description':'外部ネッワークとの通信の際には利用者1人に1つのグローバルIPアドレスに変換され、利用者別に論理的な個別プライベートネットワークを構築するNAPT。',
+    'technology':'Ruby、Trema version 0.4.6','images':['imgs/portfolio/personalNapt.png'],'url':''},
+    {'id':'work2','title':'情報処理技術者試験カレンダー','description':'情報処理技術者試験までの日数を表示するシステム','technology':'HTML5、JavaScript',
+     'images':['imgs/portfolio/calender.png'],
+     'url':'https://takuyash.github.io/calendar/'},
+    {'id':'work3','title':'利用者端末管理システム','description':'ネットワークを利用する利用者の端末を管理するシステム。','technology':'Ruby、Ruby on Rails、JavaScript、Bootstrap',
+    'images':['imgs/portfolio/terminalControl.png'],'url':''},
+    {'id':'work4','title':'パケットモニタリングシステム','description':'パケット量を表示し可視化するシステム。','technology':'Ruby、Ruby on Rails、JavaScript、Highcharts',
+    'images':['imgs/portfolio/packetmonitoring2.png','imgs/portfolio/packetmonitoring3.png','imgs/portfolio/packetmonitoring1.png'],'url':''},
+
+    {'id':'work5','title':'パケットモニタリングシステム','description':'パケット量を表示し可視化するシステム。','technology':'Ruby、Ruby on Rails、JavaScript、Highcharts',
+    'images':['imgs/portfolio/packetmonitoring2.png','imgs/portfolio/packetmonitoring3.png','imgs/portfolio/packetmonitoring1.png'],'url':''},
+
+];
