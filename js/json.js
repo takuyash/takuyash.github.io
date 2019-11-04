349d6eb8ffbf4bc7754466b6f82bf73c68f2311a
takuyash <14888534+takuyash@users.noreply.github.com>
2019-11-04 22:50:17 +0900

    説明をsyuusei

--- js/json.js
+++ js/json.js
@@ -7,7 +7,7 @@ var works = [
     {'id':'work3','title':'利用者端末管理システム','description':'ネットワークを利用する利用者の端末を管理するシステム。','technology':'Ruby、Ruby on Rails、SQLite3、JavaScript、Bootstrap',
     'images':['imgs/portfolio/terminalControl1.png','imgs/portfolio/terminalControl2.png','imgs/portfolio/terminalControl3.png','imgs/portfolio/terminalControl4.png'],'url':''},
     {'id':'work4','title':'利用者別NAPT',
-    'description':'OpenFlowを用いて、外部ネッワークとの通信の際には利用者1人に1つのグローバルIPアドレスに変換され、さらに利用者別に論理的な個別プライベートネットワークを構築するNAPT。トポロジ検出機能と最適経路決定機能も持つ。',
+    'description':'OpenFlowを用いて、外部ネッワークとの通信の際には利用者1人に1つのグローバルIPアドレスに変換され、さらに利用者別に論理的な個別プライベートネットワークを構築するNAPT。トポロジ検出機能(LLDPを使用)と最適経路決定機能(ダイクストラ法を使用)も持つ。',
     'technology':'Ruby、OpenFlow(Trema 0.4.7)','images':['imgs/portfolio/personalNapt1.png','imgs/portfolio/personalNapt2.png'],'url':''},
     {'id':'work5','title':'プライベートポートフォリオサイト','description':'個人で作成したシステムをまとめたポートフォリオサイト。','technology':'HTML5、JavaScript、Bootstrap',
     'images':['imgs/portfolio/portfoliosite1.png','imgs/portfolio/portfoliosite2.png','imgs/portfolio/portfoliosite3.png'],'url':'https://takuyash.github.io/'}, 
